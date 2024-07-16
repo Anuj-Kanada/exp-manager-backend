f96fc5c8368fe15679538c032630ea7632d62fb9
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-07-16 11:11:56 +0530

    Update ExpenseCategoryRoute.js

--- routes/ExpenseCategoryRoute.js
+++ routes/ExpenseCategoryRoute.js
@@ -4,4 +4,4 @@ const expensecategory = require('../controllers/ExpenseCategoryController')
 router.get('/expensecategory' , expensecategory.getCategory);
 router.post('/expensecategory' , expensecategory.addCategory);
 
-module.exports = router;
+module.exports = routers;
